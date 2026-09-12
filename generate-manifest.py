f4c06fc14f9919660ecad7e92420992dd918b1ae
Ushinar-career <ushinar.career@outlook.com>
2026-09-12 07:37:00 +0000

    Updated

--- generate-manifest.py
+++ generate-manifest.py
@@ -7,27 +7,22 @@ SW_FILE = os.path.join(BASE_DIR, "service-worker.js")
 
 files = []
 
-# Walk through static folder
 for root, _, filenames in os.walk(STATIC_DIR):
     for filename in filenames:
         full_path = os.path.join(root, filename)
-        rel_path = "./" + os.path.relpath(full_path, BASE_DIR).replace("\\", "/")
+        rel_path = os.path.relpath(full_path, BASE_DIR).replace("\\", "/")
         files.append(rel_path)
 
-# Build nicely formatted array with newlines
 formatted_list = "[\n" + ",\n".join(f'  "{f}"' for f in files) + "\n]"
 
-# Read service worker
 with open(SW_FILE, "r", encoding="utf-8") as f:
     sw_content = f.read()
 
-# Replace urlsToCache array
 start = sw_content.find("const urlsToCache")
 end = sw_content.find("];", start) + 2
 new_array = f"const urlsToCache = {formatted_list};"
 sw_content = sw_content[:start] + new_array + sw_content[end:]
 
-# Write back updated service worker
 with open(SW_FILE, "w", encoding="utf-8") as f:
     f.write(sw_content)
 
